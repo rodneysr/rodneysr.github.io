d9f75117a75a2c1e4d78e6fc00385ad8dfe83506
Digital Media Student <digital@browardtech.local>
2016-09-30 08:40:48 -0400

    wtf

---
+++ animated_banner/Banner 336x280_edge.js
@@ -0,0 +1,47 @@
+
+(function(compId){var _=null,y=true,n=false,x8='break-word',x2='5.0.0',e27='${RepeatBG}',x13='rgba(0,0,0,0)',e20='${DownloadButton}',g='image',e42='${RepeatBG3}',x31='45px',x40='RepeatBG3',x29='1',e37='${Button02}',x='text',m='rect',x22='0px',x33='img',x6='montserrat, sans-serif',x24='280px',i='none',x38='Flag_Back_03',x3='6.0.0.400',e36='${Button01}',p='px',e18='${SecondText}',o='opacity',x21='RepeatBG',e19='${LogoHeader}',x34='Button01',e16='${FirstText}',x28='Button02',x25='auto',x30='310px',x1='6.0.0',x23='1008px',x15='rgba(255,255,255,1)',tp='top',x11='24',e17='${ThirdText}',x7='300',lf='left',l='normal',x12='500',x5='10';var g14='LogoHeader.jpg',g39='Flag%20Back%2003.jpg',g35='Button01.png',g26='Flag%20Back%2002.jpg',g41='Flag%20Back%20032.jpg',g32='Button02.png';var s10="<p style=\"margin: 0px; text-align: center;\"><span style=\"font-family: montserrat, sans-serif; font-weight: 700; font-size: 22px;\">Proper Planning Prevents Piss Poor Performance.</span></p>",s4="<p style=\"margin: 0px; text-align: center;\">​<span style=\"font-size: 14px;\">OBTAIN ALL VETERAN DOCUMENTS FROM ONE LOCATION.</span></p><p style=\"margin: 0px;\"></p>",s9="<p style=\"margin: 0px; text-align: center;\">​<span style=\"font-size: 14px;\">DD-214, VA LOANS, VA BENEFITS, &amp; MORE.</span></p><p style=\"margin: 0px;\"></p>";var im='images/',aud='media/',vid='media/',js='js/',fonts={'montserrat, sans-serif':'<script src=\"http://use.edgefonts.net/montserrat:n4:all.js\"></script>'},opts={'gAudioPreloadPreference':'auto','gVideoPreloadPreference':'auto'},resources=[],scripts=[],symbols={"stage":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:'RepeatBG32',symbolName:'RepeatBG3',t:m,r:['0px','0','1008','280','auto','auto']},{id:'DownloadButton',symbolName:'DownloadButton',t:m,r:['13px','289px','310','45','auto','auto']},{id:'ThirdText',t:x,r:['-336px','169px','336px','36px','auto','auto'],text:s4,align:"left",n:[x6,[x5,p],"rgba(255,255,255,1)",x7,i,l,x8,l],ts:["","","","",i]},{id:'SecondText',t:x,r:['336px','136px','336px','22px','auto','auto'],text:s9,align:"left",n:[x6,[x5,p],"rgba(255,255,255,1)",x7,i,l,x8,l],ts:["","","","",i]},{id:'FirstText',t:x,r:['-336px','62px','336px','60px','auto','auto'],text:s10,n:[x6,[x11,],"rgba(255,255,255,1.00)",x12,i,"",x8,l]},{id:'LogoHeader',t:g,r:['0px','-45px','336px','45px','auto','auto'],f:[x13,im+g14,'0px','0px']}],style:{'${Stage}':{isStage:true,r:['null','null','336px','280px','auto','auto'],overflow:'hidden',f:[x15]}}},tt:{d:10000,a:y,data:[["eid47",lf,2000,500,"linear",e16,'-336px','0px'],["eid51",lf,4000,500,"linear",e17,'-336px','0px'],["eid50",lf,3000,500,"linear",e18,'336px','0px'],["eid36",lf,1000,0,"linear",e19,'0px','0px'],["eid56",lf,5000,0,"linear",e20,'13px','13px'],["eid57",lf,5500,0,"linear",e20,'13px','13px'],["eid46",tp,2000,0,"linear",e16,'62px','62px'],["eid60",tp,3000,500,"linear",e18,'145px','136px'],["eid59",tp,4000,500,"linear",e17,'185px','169px'],["eid38",tp,1000,500,"linear",e19,'-45px','0px'],["eid58",tp,5000,500,"linear",e20,'289px','221px']]}},"RepeatBG":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x21,t:g,r:[x22,x22,x23,x24,x25,x25],f:[x13,im+g26,x22,x22]}],style:{'${symbolSelector}':{r:[_,_,x23,x24]}}},tt:{d:10000,a:y,data:[["eid16",tp,10000,0,"linear",e27,'0px','0px'],["eid18",lf,0,10000,"linear",e27,'0px','-672px']]}},"DownloadButton":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:g,id:x28,o:x29,r:[x22,x22,x30,x31,x25,x25],f:[x13,im+g32,x22,x22]},{r:[x22,x22,x30,x31,x25,x25],tag:x33,id:x34,o:x29,t:g,f:[x13,im+g35,x22,x22]}],style:{'${symbolSelector}':{r:[_,_,x30,x31]}}},tt:{d:500,a:n,data:[["eid21",o,0,500,"linear",e36,'1','0'],["eid26",o,0,0,"linear",e37,'1','1']]}},"RepeatBG_03":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{id:x38,t:g,r:[x22,x22,x23,x24,x25,x25],f:[x13,im+g39,x22,x22]}],style:{'${symbolSelector}':{r:[_,_,x23,x24]}}},tt:{d:10000,a:y,data:[]}},"RepeatBG3":{v:x1,mv:x2,b:x3,stf:i,cg:i,rI:n,cn:{dom:[{t:g,id:x40,r:[x22,x22,x23,x24,x25,x25],f:[x13,im+g41,x22,x22]}],style:{'${symbolSelector}':{r:[_,_,x23,x24]}}},tt:{d:10000,a:y,data:[["eid35",lf,0,10000,"linear",e42,'0px','-672px'],["eid32",tp,10000,0,"linear",e42,'0px','0px']]}}};AdobeEdge.registerCompositionDefn(compId,symbols,fonts,scripts,resources,opts);})("EDGE-335412089");
+(function($,Edge,compId){var Composition=Edge.Composition,Symbol=Edge.Symbol;Edge.registerEventBinding(compId,function($){
+//Edge symbol: 'stage'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",10000,function(sym,e){});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${DownloadButton}","mouseover",function(sym,e){sym.getSymbol("DownloadButton").play();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${DownloadButton}","mouseout",function(sym,e){sym.getSymbol("DownloadButton").playReverse();});
+//Edge binding end
+Symbol.bindElementAction(compId,symbolName,"${DownloadButton}","click",function(sym,e){window.open("http://sixpee.businesscatalyst.com/","_blank");});
+//Edge binding end
+})("stage");
+//Edge symbol end:'stage'
+
+//=========================================================
+
+//Edge symbol: 'RepeatBG'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",10000,function(sym,e){this.play(0);});
+//Edge binding end
+})("RepeatBG");
+//Edge symbol end:'RepeatBG'
+
+//=========================================================
+
+//Edge symbol: 'DownloadButton'
+(function(symbolName){Symbol.bindElementAction(compId,symbolName,"${Button01}","mouseover",function(sym,e){});
+//Edge binding end
+})("DownloadButton");
+//Edge symbol end:'DownloadButton'
+
+//=========================================================
+
+//Edge symbol: 'RepeatBG_03'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",10000,function(sym,e){this.play(0);});
+//Edge binding end
+})("RepeatBG_03");
+//Edge symbol end:'RepeatBG_03'
+
+//=========================================================
+
+//Edge symbol: 'RepeatBG3'
+(function(symbolName){Symbol.bindTriggerAction(compId,symbolName,"Default Timeline",10000,function(sym,e){this.play(0);});
+//Edge binding end
+})("RepeatBG3");
+//Edge symbol end:'RepeatBG3'
+})})(AdobeEdge.$,AdobeEdge,"EDGE-335412089");
